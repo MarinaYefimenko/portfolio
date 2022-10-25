d618f6a1191a0ed3e0fa4946e69db2f1b7635ae1
MYefimenko <marinka0410@gmail.com>
2022-10-25 22:24:06 +0200

    baseHTML

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        light: '#e9e7e7',
+        grey: '#d7d6d6',
+        grdark: '#515155',
+        dark: '#232323'
+      }
+    },
+  },
+  plugins: [],
+}
